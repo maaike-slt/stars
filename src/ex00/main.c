4ee7e31aa0e3fad2945a8952d501c92ea2b02653
Alexdelia <alexandre.delille.57@gmail.com>
2023-03-20 10:54:13 +0100

    norm

--- src/ex00/main.c
+++ src/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:13:19 by adelille          #+#    #+#             */
-/*   Updated: 2023/03/20 10:50:39 by adelille         ###   ########.fr       */
+/*   Updated: 2023/03/20 10:54:05 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@ int	answer(void);
 
 int	main(void)
 {
-	return !(answer() == 42);
+	return (!(answer() == 42));
 }
