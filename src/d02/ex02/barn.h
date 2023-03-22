fdc1c3e0682a99e6df9706e023e295c050909631
Alexdelia <alexandre.delille.57@gmail.com>
2023-03-22 19:37:52 +0100

    `ex02`

---
+++ src/d02/ex02/barn.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   barn.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 19:23:11 by adelille          #+#    #+#             */
+/*   Updated: 2023/03/22 19:29:35 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BARN_H
+# define BARN_H
+
+#include <stdbool.h>
+
+unsigned int	special_sum(unsigned int tree);
+
+bool	can_feed(unsigned int tree, unsigned int pony);
+
+#endif
---
+++ src/d02/ex02/can_feed.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   can_feed.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 19:33:45 by adelille          #+#    #+#             */
+/*   Updated: 2023/03/22 19:37:12 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdbool.h>	// you need to include stdbool.h to have bool type
+
+#include "barn.h"	// #include <stdbool.h> is already in the file barn.h
+// so no need to include 2 times
+
+bool	can_feed(unsigned int tree, unsigned int pony)
+{
+	int	apple;
+
+	apple = special_sum(tree);
+	/*
+		your code
+		...
+	*/
+	return (false);	// return your solution
+}
