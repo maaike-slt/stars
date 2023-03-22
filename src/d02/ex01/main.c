593c672cab80b8ddabcb671df9bfdc4f2c3504d0
Alexdelia <alexandre.delille.57@gmail.com>
2023-03-22 10:54:52 +0100

    some elf magic

--- src/d02/ex01/main.c
+++ src/d02/ex01/main.c
@@ -6,17 +6,19 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:13:19 by adelille          #+#    #+#             */
-/*   Updated: 2023/03/22 10:48:25 by adelille         ###   ########.fr       */
+/*   Updated: 2023/03/22 10:54:36 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	sum(unsigned int tree);
+unsigned int	special_sum(unsigned int n);
 
 int	main(void)
 {
 	return (!(
-			sum(0) == 0
-			&& sum(3) == 5
-			&& sum(5) == 15
+			special_sum(0) == 0
+			&& special_sum(4) == 10
+			&& special_sum(5) == 9
+			&& special_sum(6) == 21
+			&& special_sum(7) == 16
 		));
 }
