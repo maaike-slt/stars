725a89c9b7de750a25b8104fafd1c621a8040720
Alexdelia <alexandre.delille.57@gmail.com>
2023-03-22 19:40:21 +0100

    CI and norm

--- src/d02/ex02/barn.h
+++ src/d02/ex02/barn.h
@@ -6,17 +6,17 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 19:23:11 by adelille          #+#    #+#             */
-/*   Updated: 2023/03/22 19:29:35 by adelille         ###   ########.fr       */
+/*   Updated: 2023/03/22 19:40:03 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BARN_H
 # define BARN_H
 
-#include <stdbool.h>
+# include <stdbool.h>
 
 unsigned int	special_sum(unsigned int tree);
 
-bool	can_feed(unsigned int tree, unsigned int pony);
+bool			can_feed(unsigned int tree, unsigned int pony);
 
 #endif
