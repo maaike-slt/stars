d0d1aeae3caf27b6120fcc025353571b7e3e39c0
Alexdelia <alexandre.delille.57@gmail.com>
2023-04-01 11:58:04 +0200

    `ex01`

---
+++ src/d03/ex01/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 10:13:19 by adelille          #+#    #+#             */
+/*   Updated: 2023/04/01 11:57:57 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+ssize_t	find_char(char *word, char letter);
+
+int	main(void)
+{
+	return (!(
+			find_char("hello", 'e') == 1
+			&& find_char("world", 'w') == 0
+			&& find_char("last", 't') == 3
+			&& find_char("but wait, what if?", '?') == 17
+			&& find_char("banana", 'n') == 3
+			&& find_char("banana", 'e') == -1
+			&& find_char("", 'a') == -1
+		));
+}
