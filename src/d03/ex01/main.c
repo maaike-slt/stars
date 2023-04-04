5e173ba0b23b67cbb252bdb90b7677b275e7ab26
maaike-slt <maaikesloot@home.nl>
2023-04-04 16:52:23 +0200

    ex01

--- src/d03/ex01/main.c
+++ src/d03/ex01/main.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:13:19 by adelille          #+#    #+#             */
-/*   Updated: 2023/04/03 17:49:42 by adelille         ###   ########.fr       */
+/*   Updated: 2023/04/04 16:51:06 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(void)
 			&& find_char("world", 'w') == 0
 			&& find_char("last", 't') == 3
 			&& find_char("but wait, what if?", '?') == 17
-			&& find_char("banana", 'n') == 3
+			&& find_char("banana", 'n') == 2
 			&& find_char("banana", 'e') == -1
 			&& find_char("", 'a') == -1
 		));
