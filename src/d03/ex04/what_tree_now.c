8585b5c9fcaa3d0ccda6e4ba8ee8df50f96e55d0
maaike-slt <maaikesloot@home.nl>
2023-04-10 21:13:49 +0200

    ex04

--- src/d03/ex04/what_tree_now.c
+++ src/d03/ex04/what_tree_now.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 17:55:56 by msloot            #+#    #+#             */
-/*   Updated: 2023/04/10 18:16:18 by msloot           ###   ########.fr       */
+/*   Updated: 2023/04/10 21:08:25 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,36 @@
 
 #include <unistd.h>
 
-size_t	what_tree_now(const uint8_t *tree_row, size_t size)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < size)
-	{
-		i++;
-	}
-	return (0);
-}
-
 void	print_tree_h(uint8_t h)
 {
 	char	c;
 
-	c = h - 'o';
+	c = h + '0';
 	write(1, &c, 1);
 	write(1, " ", 1);
+}
+
+size_t	what_tree_now(const uint8_t *tree_row, size_t size)
+{
+	size_t	i;
+	size_t	count;
+	uint8_t	h;
+
+	i = 0;
+	count = 0;
+	h = 0;
+	while (i < size)
+	{
+		if (tree_row[i] > h)
+		{
+			h = tree_row[i];
+			count++;
+		}
+		print_tree_h(tree_row[i]);
+		print_tree_h(h);
+		write(1, "\n", 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	return (count);
 }
